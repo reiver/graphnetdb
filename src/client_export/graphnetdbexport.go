89439a66b08bac5a2dd81da66b1a303a66d9ad7b
Charles <charles@reptile.ca>
2013-03-20 12:31:25 -0700

    placeholder to graphnetdbexport

---
+++ src/client_export/graphnetdbexport.go
@@ -0,0 +1,24 @@
+// This program connects to the graphnetdb server, and exports a graph or part of a graph.
+
+package main
+
+
+import (
+    "fmt"
+)
+
+
+func main() {
+    //TEST
+        fmt.Printf("nodedef> name\n")
+        fmt.Printf("a\n")
+        fmt.Printf("b\n")
+        fmt.Printf("c\n")
+        fmt.Printf("d\n")
+        fmt.Printf("e\n")
+        fmt.Printf("edgedef> node1,node2\n")
+        fmt.Printf("a,b\n")
+        fmt.Printf("b,c\n")
+        fmt.Printf("b,d\n")
+        fmt.Printf("d,e\n")
+}
